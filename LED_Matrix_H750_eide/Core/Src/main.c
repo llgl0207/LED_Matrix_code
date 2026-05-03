dc2c593a24d187cda2cce2a200047fb8b0e9aac2
浪里个浪 <120241584+llgl0207@users.noreply.github.com>
2026-05-03 20:14:47 +0800

    temp

--- LED_Matrix_H750_eide/Core/Src/main.c
+++ LED_Matrix_H750_eide/Core/Src/main.c
@@ -25,7 +25,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-#include "stm32h7xx.h"
+#include "tim.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
